e6879de0b2b1b77113353b9115c56c6acb075b54
Ricardo Hurtado <ricardo040400@gmail.com>
2023-04-10 22:55:06 -0400

    responsive  layout basico para telefonos

--- src/components/sections/home/Home.js
+++ src/components/sections/home/Home.js
@@ -10,19 +10,14 @@ const Home = (props) => {
   return (
     <section className="section" data-label="Home" id="home">
       <header id="head">
-        <DividerLetter word={"Ricardo"} />
-        <DividerLetter word={"Hurtado"} />
+        <DividerLetter style={"letters-container"} word={"Ricardo"} />
+        <DividerLetter style={"letters-container"} word={"Hurtado"} />
       </header>
       <section id="decoration-description">
         <Decoration size={40} />
         <span className="description">{description}</span>
       </section>
-      <div id="hero">
-        <ImageHero size={43} type="0" deg="0" />
-        <ImageHero size={40} type="2" deg="0" />
-        <ImageHero size={48} type="1" deg="0" />
-        <ImageHero size={39} type="" deg="-5" />
-      </div>
+      <div id="hero"></div>
     </section>
   );
 };
