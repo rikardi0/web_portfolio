a4cc53da09eed31d69039495478cdd8da89773f7
Ricardo Hurtado <ricardo040400@gmail.com>
2023-05-02 01:12:53 -0400

    archivos de recursos (hardcoded data)

---
+++ src/components/sections/res/strings/list.js
@@ -0,0 +1,42 @@
+import Images from "../images/index";
+
+const projectContent = {
+  spanish: {
+    projectList: [
+      {
+        path: Images.close_tab,
+        slideImg: [
+          {
+            url: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+          },
+          {
+            url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+          },
+        ],
+        title: "PIPIPI ",
+        description: "Lo mejor de lo mejor",
+        technologies: ["a", "b", "c"],
+      },
+    ],
+  },
+  english: {
+    projectList: [
+      {
+        path: Images.close_tab,
+        slideImg: [
+          {
+            url: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+          },
+          {
+            url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+          },
+        ],
+        title: "PIPIPI INGLES",
+        description: "Lo mejor de lo mejor",
+        technologies: ["a", "b", "c"],
+      },
+    ],
+  },
+};
+
+export default projectContent;
---
+++ src/components/sections/res/strings/strings.js
@@ -0,0 +1,39 @@
+const rightArrow = String.fromCharCode(parseInt("2794", 16));
+const downArrow = String.fromCharCode(parseInt("21F2", 16));
+const homeArrow = String.fromCharCode(parseInt("21D4", 16));
+const strings = {
+  body: {
+    english: {
+      description:
+        "I'm an a mobile app developer with experience making apps using Java and Flutter, I'm always looking to expand my skills",
+      aboutTitle: "About-Me",
+      projectsTitle: "Projects",
+      skillTitle: "skill-set",
+      contact: "Let's team up!",
+      about:
+        "I'm an 23-year-old mechanical engineer with a passion for app and web development. I specialize in flutter and java, and my proficiency in design sets me apart from the competition. Let's work together to bring your vision to life!",
+      skill: [
+        `Flutter${rightArrow}`,
+        `J a v a ${downArrow}`,
+        `Front-end ${homeArrow}`,
+      ],
+    },
+    spanish: {
+      description:
+        "Soy un desarrollador de aplicaciones móviles con experiencia en Java y Flutter, siempre estoy buscando expandir mis habilidades.",
+      aboutTitle: "Sobre-Mi",
+      projectsTitle: "Proyectos",
+      skillTitle: "habilidad",
+      contact: "Hagamos Eqiupo!",
+      about:
+        "Soy un ingeniero mecánico de 23 años apasionado por el desarrollo web y de aplicaciones. Me especializo en flutter y java, y mi competencia en diseño me diferencia. ¡Trabajemos juntos para cumplir su visión!",
+      skill: [
+        `Flutter${rightArrow}`,
+        `J a v a ${downArrow}`,
+        `Front-end ${homeArrow}`,
+      ],
+    },
+  },
+};
+
+export default strings;
