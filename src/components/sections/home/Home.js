908c9de06726491a973e8a796b7b06e5de6550fa
Ricardo Hurtado <ricardo040400@gmail.com>
2023-05-02 21:06:40 -0400

    Ajustes UI (dimensiones)

--- src/components/sections/home/Home.js
+++ src/components/sections/home/Home.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Decoration from "../res/svg/Starts";
 import ImageHero from "../res/svg/ImageHero";
 import DividerLetter from "../../ui/DividerLetter";
-
 import "./Home.css";
 const Home = (props) => {
   const { description } = props;
@@ -32,10 +31,10 @@ const Home = (props) => {
         </div>
       ) : (
         <div id="hero">
-          <ImageHero size={290} type={"0"} deg={0} />
-          <ImageHero size={270} type={"2"} deg={0} />
-          <ImageHero size={310} type={"1"} deg={0} />
-          <ImageHero size={300} type={""} deg={-5} />
+          <ImageHero size={310} type={"0"} deg={0} />
+          <ImageHero size={290} type={"2"} deg={0} />
+          <ImageHero size={350} type={"1"} deg={0} />
+          <ImageHero size={325} type={""} deg={-5} />
         </div>
       )}
     </section>
---
+++ src/components/sections/res/svg/Minimize.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Minimize() {
+  return (
+    <svg
+      className="minimize"
+      width="20px"
+      height="20px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M6 11H18V13H6V11Z" fill="#000000" />
+    </svg>
+  );
+}
+
+export default Minimize;
